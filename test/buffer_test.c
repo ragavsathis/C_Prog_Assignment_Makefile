4f61d41eef8cb5691775e28b91ecb47c1a6b7e49
RCHINNU <ragavsathis.chinnu@volvocars.com>
2021-12-07 17:29:08 +0100

    checking the tag

--- test/buffer_test.c
+++ test/buffer_test.c
@@ -26,6 +26,7 @@ void test_buffer_insert_extract_on_single_byte(void)
     TEST_ASSERT_EQUAL(144, value);
 
     // After buffer inserting, array elements would be changed as shown in comments
+    //
     TEST_ASSERT_EQUAL(64, sample_arr[0]);
     TEST_ASSERT_EQUAL(2, sample_arr[1]);
     TEST_ASSERT_EQUAL(3, sample_arr[2]);
